2118671e361ce49ccb00cc3c8d947a1f72dd4080
Joshua Conner <joshuaconner@nau.edu>
2012-04-13 16:32:09 -0700

    Adding prototype screenshots

---
+++ Circle/Circle/CircleEventDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CircleEventDetailsViewController.h
+//  Circle
+//
+//  Created by Joshua Conner on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface CircleEventDetailsViewController : PFQueryTableViewController
+
+@end
